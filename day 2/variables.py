25c02f082b90e6cc3d98b25d0c91df6d86a35d8e
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-01-21 23:03:11 +0530

    Added variables.py

---
+++ day 2/variables.py
@@ -0,0 +1,28 @@
+""" variables in python?
+variable is a name to store data in a memory.in python no need to declare the type of variable;
+python automatically assigns the datatype based on the value.
+ex:  x=10
+     name="keerthi"
+Rules to declare variable:
+     1.variable name should start witha a letter or underscore(_)
+     2.variable name should not start witha a number
+     3.variable can contain only letters,numbers and underscore(_)
+     4.variable names are case sensitive
+     5.keywords cannot be used as variable names
+     6.variable name should be meaningful
+Types of variables:
+1.local variable
+    "Declared inside a function and used only within that function"
+2.global variable
+    "Declared outside the function and can be used anywhere in the program"
+3.instance variable
+    "Declared inside a class and used to store data for each object of the class"
+4.class variable
+    "shared among all the objects of a class"
+Use of variables:
+1.store data
+2.reuse data
+3.make code readable
+4.easy to update data
+5.perform calculations
+"""
