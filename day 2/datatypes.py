798b60b1297630cc5e162fbd903e3d8cf15ecef4
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-01-22 08:39:00 +0530

    added datatypes.py

---
+++ day 2/datatypes.py
@@ -0,0 +1,32 @@
+""" what is datatype?
+       Datatype specifies the type of data that a variable can hold.
+       ex:integer,float,string,boolean,list,tuple,dictionary,set
+    why datatype is important?
+        tell python how to store data in memory
+        define what operations can be allowed on the data
+        help avoid errors
+        make programs more efficient
+    types of datatypes in python:
+    1.numeric datatypes
+        stores numbers
+        a.integer(int):whole numbers(positive,negative,zero)
+        ex:x=200
+        b.float:decimal numbers,ex:y=3.45
+        c.complex:numbers with real and imaginary parts,ex:z=2+3j
+    2.text datatype
+        a.string(str):stores text or characters,ex:namme="keerthi"
+    3.boolean datatype
+        a.boolean(bool):stores true or false values,ex:flag=true
+    4.sequence datatypes
+        stores multiple values in a specific order
+        a.list:mutable list of items enclosed in square brackets,ex:nums=[1,2,3,4]
+        b.tuples:immutable list of items enclosed in parameters,ex:nums=(1,2,3)
+        c.range:represents a sequence of numbers,exrange(1,10)
+    5.set datatype
+       set:unordered ,unique values enclosed in curly braces,ex=nums={1,2,3}
+       frozenset:unchangeable set of unique values,ex:nums=frozenset({1,2,3})
+    6.mapping datatype
+       dictionary(dict):stores key-value pairs enclosed in curly braces,ex:dict={"name":"keerthi","age":20}
+    7.none datatype
+       none:represents no value,ex:x=none
+       """
