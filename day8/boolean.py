a14ac675e1c987b26b048587075266e0c6bcc0a3
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-01-29 22:24:02 +0530

    Added programs on booleans

--- day8/boolean.py
+++ day8/boolean.py
@@ -18,3 +18,50 @@ booleans are commonly used in :
 1. Conditional Statements:
 2. Loops
 3. Comparisons
+
+#Boolean from comparisons
+booleans are often the result of comaprison operators
+#ex:
+x=2
+y=8
+print(x>=y)#False
+print(x==y)#False
+print(x<=y)#True
+
+#Boolean with logical operators
+Logical operators work with boolean values
+and:True if both conditions are True
+or:True if atleast one condition is True
+not:reverse the result
+#ex:
+print(True and True)#True
+print(True or False)#True
+print(not True)#False
+
+#Boolean in conditional statements
+Booleans is mainly used in if else conditions
+#ex:
+age=18
+if age>=18:
+    print("Eligible to vote")#True
+else:
+    print("Not eligible")    
+
+#Truthy and Falsy values
+some values are treated as false automatically
+#0
+#none
+#""
+#[],{},()
+everything else is treated as True
+
+#ex:
+print(bool(0))#False
+print(bool("hi"))#True
+
+
+
+
+
+
+
---
+++ day8/programs on booleans.py
@@ -0,0 +1,37 @@
+#1.check greater number
+"""x=int(input("Enter a num1:")) #20
+y=int(input("Enter a num2:")) #5
+print(x>y) #True
+print(x<y) #False
+
+#2.even or odd using boolean
+num=30
+print(num%2==0) #True
+
+#3.voting eligibility
+age=18
+print(age>=18)
+
+#4.check login
+name="keerthi"
+age="20"
+yr="20"
+print(name==age and age==yr)#False
+
+#5.Boolean with bool() function
+print(bool(0)) #False
+print(bool("")) #False
+print(bool("hi")) #True
+print(bool(" ")) #True
+print(bool()) #False
+print(bool(20)) #True
+print(bool([])) #FalseS"""
+
+#6.Display Boolean values
+x=True
+y=False
+print(x)
+print(y)
+print(int(x))
+print(int(y))
+
