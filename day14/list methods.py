fd620a6aa528eea5966c2738c8409e7aa4e64e10
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-02-08 17:47:07 +0530

    Added list methods

---
+++ day14/list methods.py
@@ -0,0 +1,62 @@
+#methods for removing elements from list
+"""li=[10,20,30,40]
+li.remove(30)#remove value
+li.pop()#remove last element
+li.pop(2)#remove index 2 value
+li.clear()#remove all elements
+
+#methods for searching
+#index()
+returns the index of first occurence of a value
+
+#ex:
+li=[2,3,4,5]
+print(li.index(4))
+
+#count()
+count how many times a value appears
+#ex:
+li=[1,2,3,1,4]
+print(li.count(1))#2
+
+#methods for sorting and reversing
+#sort()
+sort the list in ascending order
+#ex:
+li=[3,4,1,2]
+li.sort()
+print(li)#[1,2,3,4]
+
+#reverse()
+reverse the list order
+#ex:
+li=[1,2,3,4]
+li.reverse()
+print(li)#[4,3,2,1]
+
+#copying method
+creates a shallow copy of list
+#ex:
+li=[1,2,3,4]
+print(li.copy())#[1,2,3,4]
+
+#len() (builtin function not methods)
+returns the length of list
+#ex:
+li=[1,2,4,5]
+print(len(li))#
+
+#max() and min()
+li=[1,2,3,4]
+print(max(li))#4
+print(min(li))#1"""
+
+#sum()
+li=[1,2,3,4]
+print(sum(li))
+
+
+
+
+
+
