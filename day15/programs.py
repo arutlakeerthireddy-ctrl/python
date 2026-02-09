c8f52e27b9d2b70fb9144916a047d122fcc52d01
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-02-10 00:13:46 +0530

    Added programs

---
+++ day15/programs.py
@@ -0,0 +1,24 @@
+#Write a program to find the sum of all elements in a list.
+li=[10,20,30,40]
+print(sum(li))#100
+
+#Write a program to find the average of list elements.
+li=[10,20,30,40]
+total=sum(li)#100
+avg=total/len(li)#100/4
+print(avg)#25.0
+
+#Write a program to multiply all elements in a list.
+li=[1,2,3,4]
+x=1
+for i in li:
+    x=x*i
+    
+print(x)#24
+
+#Write a program to separate even and odd numbers from a list.
+
+#Write a program to find the difference between max and min elements.
+li=[1,2,3,4]
+difference=max(li)-min(li)
+print(difference)
