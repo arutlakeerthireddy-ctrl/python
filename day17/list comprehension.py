ac6c91eedac15afc80a32f0f6488f482646de858
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-02-15 00:00:05 +0530

    added

--- day17/list comprehension.py
+++ day17/list comprehension.py
@@ -1,5 +1,5 @@
 #list comprehension
-"""list comprehension is a short and powerful way to create new list
+list comprehension is a short and powerful way to create new list
 using a single line of code.
 instead of writing multiple lines within a loop,we write it in one clean expression.
 
@@ -17,11 +17,12 @@ print(even)#[0,2,4,6,8]
 
 #with if-else
 [expression_if_true if condition else expression_if_false for item in iterable]
-#ex"""
+#ex
 result=["even" if x%2==0 else "odd" for x in range(5)]
 print(result)#['even','odd','even','odd','even']
 
 #using list comprehension on existing list
 li=[10,20,30]
 new=[x+5 for x in li]
-print(new)
+print(new)#[15,25,35]
+
---
+++ day18/change tuple.py
@@ -0,0 +1,6 @@
+#change tuple value
+t=("hello","iam","keerthi")
+li=list(t)
+li[1]="arutla"
+t1=tuple(li)
+print(t1)
