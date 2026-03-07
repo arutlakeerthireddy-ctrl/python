e5e319e1945bc08a2495eff5f8ae121226d11f1e
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-03-07 23:42:05 +0530

    added programs

---
+++ day33/programs on for loop.py
@@ -0,0 +1,11 @@
+#Write a program to print all elements in a list using a for loop.
+li=[1,2,3,4,5]
+for i in range(len(li)):
+    print(i)
+
+#Write a program to find the sum of all elements in a list.
+li=[1,2,3,4,5]
+sum=0
+for i in range(0,6):
+    sum+=i
+print(sum)
