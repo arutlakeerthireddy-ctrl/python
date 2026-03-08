2b0c67db390a04dfdc34d190d7bbd537a64dd4fc
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-03-08 22:22:13 +0530

    added programs

---
+++ day34/prog.py
@@ -0,0 +1,11 @@
+#Write a program to print the cube of numbers from 1 to 10.
+"""for i in range(1,11):
+    print(i**3)"""
+
+#Write a program to find the sum of even numbers between 1 and 100.
+sum=0
+for i in range(1,101):
+    if i%2==0:
+        sum+=i
+print(sum)
+
