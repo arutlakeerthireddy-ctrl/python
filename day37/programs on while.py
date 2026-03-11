73e0fa2ffd60ad8e14621baf3ac9804a90a40396
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-03-11 15:19:21 +0530

    added programs on while loop

---
+++ day37/programs on while.py
@@ -0,0 +1,18 @@
+#Write a program to print numbers from 1 to 10 using a while loop.
+i=1
+while i<=10:
+    print(i)
+    i+=1
+
+#Write a program to print numbers from 10 to 1 in reverse order using a while loop.
+i=10
+while i>=1:
+    print(i)
+    i-=1
+
+#Write a program to print all even numbers from 1 to 50 using a while loop.
+i=2
+while i<=50:
+    print(i)
+    i+=2
+    
