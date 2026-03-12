61fffc7164c4748ad015a7f9e13bd463b43e81be
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-03-12 12:29:51 +0530

    added programs

---
+++ day38/programs.py
@@ -0,0 +1,21 @@
+#Write a program to find the factorial of a number using a while loop.
+n=int(input("Enter number:"))
+fact=1
+i=1
+while i<=n:
+    fact=fact*i
+    i+=1
+print(fact)
+
+#Write a program to print the Fibonacci series up to n terms using a while loop.
+n=int(input("Enter no. of terms:"))
+a=0
+b=1
+i=1
+while i<=n:
+    print(a)
+    c=a+b
+    a=b
+    b=c
+    i+=1
+
