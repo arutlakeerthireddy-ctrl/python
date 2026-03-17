7a5601f1e1ece15d41f93fd4da97187216fde180
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-03-17 16:45:16 +0530

    added program

---
+++ day43/programs.py
@@ -0,0 +1,9 @@
+#Write a program to count the number of equal pairs (i, j) in a list
+#  where i < j and list[i] == list[j].
+li=[1,2,3,4,5,2,3,4]
+count=0
+for i in range(len(li)):
+    for j in range(i+1,len(li)):
+        if i<j and li[i]==li[j]:
+            count+=1
+print(count)
