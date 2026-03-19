e44d664ee47ccf2d9b62ab9d60eed30850075701
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-03-19 20:34:24 +0530

    added programs

---
+++ day45/programs.py
@@ -0,0 +1,19 @@
+#Write a program using a while loop that stops execution when the number reaches 6 using break.
+"""i=1
+while i>0:
+    if i==6:
+        break
+    print(i)
+    i+=1"""
+
+#Write a program using a while loop to skip multiples of 3 using continue.
+i=1
+while i<=20:
+    if i%3==0:
+        i+=1
+        continue
+    print(i)
+    i+=1
+    
+
+#Write a program with an empty function using the pass statement.
