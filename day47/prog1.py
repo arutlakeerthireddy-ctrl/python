d05e1e7af976dfc7d6c74602a4c1ee1c5c63cc8a
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-03-21 11:51:19 +0530

    added patterns

---
+++ day47/prog1.py
@@ -0,0 +1,12 @@
+#Print a rectangle pattern (rows ≠ columns).
+
+rows=4
+columns=6
+for i in range(rows):      
+    print("* "*columns)
+
+"""output
+* * * * * * 
+* * * * * * 
+* * * * * *
+* * * * * *"""
