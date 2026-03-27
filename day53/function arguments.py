4fa0486d9aedde64090db67df67e3c0ec63692d5
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-03-27 18:55:30 +0530

    added function arguments

---
+++ day53/function arguments.py
@@ -0,0 +1,20 @@
+#function arguments:values you pass to a function
+#ex
+def add(a,b):
+    return a+b
+print(add(3,4)) #7
+
+#parameters-a,b(variables inside function)
+#arguments-2,3(actual values)
+
+#types of arguments
+#1.positional arguments
+#2.keyword arguments
+#3.default arguments
+#4.variable length arguments
+#*args
+#**kwargs(keyword variable length arguments)
+#5.postional-only arguments
+#6.keyword-only arguments
+#7.mixed arguments
+#8.argument unpacking
