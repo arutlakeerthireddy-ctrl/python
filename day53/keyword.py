d3754bbbbde72a8c4970638ce4f07b289615c176
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-03-27 19:52:13 +0530

    added arguments

---
+++ day53/keyword.py
@@ -0,0 +1,22 @@
+#keyword arguments
+#arguments that pass to a function using parameter names (dont have order)
+#examples
+def greet(name,age):
+    print(name,age)
+greet(name="keerthi",age=21) #keerthi 21
+
+def rectangle(len,bre):
+    print(len*bre)
+rectangle(len=5,bre=6) #30
+rectangle(bre=6,len=5) #30
+
+def fun(a,b):
+    print(a+b)
+x=5
+y=4
+fun(b=x,a=y) #9  order doesnt matter
+
+
+
+
+
