c0a29260cee254bb44f39c1ad9d60b5b13ca5351
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-04-08 12:50:20 +0530

    added programs

---
+++ day62/programs.py
@@ -0,0 +1,45 @@
+#Write a program to find the minimum value in a list using min().
+li=[4,5,8,0,1,3]
+def minimum_value():
+    return min(li)
+print(minimum_value())
+
+#Use abs() to convert negative numbers to positive.
+def convert(a):
+    return abs(a)
+print(convert(-2)) #2
+
+#Write a program using round() to round decimal values.
+f=10.6
+def decimal_values():
+    return round(f)
+print(decimal_values())
+
+#Use sorted() on a list and a string.
+li=[4,7,8,1,2,0,4]
+def sorting_values():
+    return sorted(li)
+print(sorting_values())
+ 
+s='b','gjfgk','a','p','hi'
+def sorting_string():
+    return sorted(s)
+print(sorting_string())
+
+#Convert a string to integer using int() and handle invalid input.
+a='56'
+
+def convert_str_int():
+    b=int(a)
+    print(b)
+convert_str_int()
+
+s='hi'
+def convert_str_int():
+    st=int(s)
+    print(st)
+convert_str_int() #invalid
+
+
+
+
