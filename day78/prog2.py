1271b6a12bb4ab41327d0634b8a44ed9720ec8c1
Keerthi Arutla <arutlakeerthireddy@gmail.com>
2026-05-09 23:14:26 +0530

    Add comment about slicing in is_palindrome function
    Updated the is_palindrome function to include a comment about using slicing.

--- day78/prog2.py
+++ day78/prog2.py
@@ -1,8 +1,8 @@
-#Write a function is_palindrome(n) that takes an integer n as input and returns True 
+#Write a function is_palindrome(n) that takes an integer n as input and returns True
 # if the number is a palindrome, otherwise returns False.
 def is_palindrome(n):
     s=str(n)
-    if s==s[::-1]:
+    if s==s[::-1]:(using slicing)
         return True
     else:
         return False
